39be3be5cadd24ebfc3851dd7d0ed95c64203d66
Muaaz [Muhammadaziz] <152799823+github-muaaz@users.noreply.github.com>
2025-01-16 14:28:42 +0500

    Update layoutManager.js

--- src/layout/layoutManager.js
+++ src/layout/layoutManager.js
@@ -5,7 +5,7 @@ import Button from "../components/elements/button";
 
 const LayoutManager = () => {
 
-    const [chat_id, setChatId] = useState(null);
+    const [chat_id] = useState(null);
 
   useEffect(() => {
     // Ensure the Telegram WebApp object is available
@@ -26,31 +26,8 @@ const LayoutManager = () => {
 
     const Layout = layouts[chat_id % 2 === 0 ? 'new' : "old"];
 
-    const randomChatId = () => {
-        setChatId(parseInt(Math.random() * (100000 - 1000) + 1000))
-    }
-
     return (
-        <React.Fragment>
-            <Layout/>
-
-
-                <Button
-                    sx={{
-                        width: 'fit-content',
-                        backgroundColor: 'yellow',
-                        position: 'absolute',
-                        top: '60px',
-                        right: '0px',
-                        fontSize: '10px !important',
-                        padding: '5px 10px !important'
-                    }}
-                    onClick={randomChatId}
-                >
-                    Change Chat Id =>
-                    {chat_id}
-                </Button>
-        </React.Fragment>
+      <Layout/>
     )
 }
 
